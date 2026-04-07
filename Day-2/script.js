706b79336ba7d83d41cb6102e241525cc74b6249
DhanushyaS01 <sdhanushyasaravanan@gmail.com>
2026-04-07 21:25:15 +0530

    feat: add user cards with box model styling and API integration

---
+++ Day-2/script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(user => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <h3>${user.name}</h3>
+        <p>${user.email}</p>
+        <p>${user.address.street}, ${user.address.city}</p>
+        <p>Im a working professional seeking a job in the field of web development.</p>
+        <button>View Profile</button>
+      `;
+
+      container.appendChild(card);
+    });
+  });
